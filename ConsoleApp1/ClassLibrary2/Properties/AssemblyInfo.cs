5ec1de93c7d57663064c7b4e6c3adf55f8df7a6f
LIDOR-PC <lidor206@gmail.com>
2017-04-05 21:39:52 +0300

    try 2

--- ConsoleApp1/ClassLibrary2/Properties/AssemblyInfo.cs
+++ ConsoleApp1/ClassLibrary2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ClassLibrary1")]
+[assembly: AssemblyTitle("ClassLibrary2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClassLibrary1")]
+[assembly: AssemblyProduct("ClassLibrary2")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("275396a5-c6d9-4b06-8484-b7db18a291ac")]
+[assembly: Guid("6b4bb6d0-17ab-49bc-bef9-40f65b143897")]
 
 // Version information for an assembly consists of the following four values:
 //
